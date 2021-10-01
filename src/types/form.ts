8b622d8b340149338f92e5812642c36ab97685a4
Kevin Dev <kevin.developer71@gmail.com>
2021-10-01 14:25:48 -0600

    feat: new TS interfaces added

--- src/types/form.ts
+++ src/types/form.ts
@@ -1,10 +1,21 @@
+import { IHeader } from "./header";
+
 export interface ILoginForm {
   state: {
-    email: string
-    password: string
+    user: {
+      email: string
+      password: string
+      username: string
+    },
+    isRegisterMode: boolean
   }
-  template: () => string
-  initialize: () => void
+  renderForm: () => string
+  loginInputs: () => string
+  registerInputs: () => string
+  initialize: (arg0?: IHeader) => void
   onSubmit: (e: Event) => void
   handleChange: (e: Event) => void
+  setRegisterMode: () => void
+  addFormListeners: () => void
+  updateFormUI: () => void
 }
