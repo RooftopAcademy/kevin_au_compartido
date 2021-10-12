a0b951d1a4f04ddee07a0a533863eeaaaae998ff
Kevin Dev <kevin.developer71@gmail.com>
2021-10-11 21:40:14 -0600

    typo

--- src/app.ts
+++ src/app.ts
@@ -101,65 +101,9 @@ document.addEventListener("DOMContentLoaded" , async () => {
   document.addEventListener('click', globalEvents)
 })
 
-const sortParams:sortParamsType = {
-  price: 1,
-  category: 1,
-  name: 1,
-  id: 1
-}
-
-const products: IProduct[] = [
-  {
-      "id": "1",
-      "imgUrl": "assets/img/alienware4.png",
-      "name": "Alienware m15",
-      "category": "Laptop",
-      "price": "999.99"
-  },
-  {
-      "id": "2",
-      "imgUrl": "assets/img/alienware3.png",
-      "name": "Alienware R4",
-      "category": "Gaming",
-      "price": "1399.89"
-  },
-  {
-      "id": "3",
-      "imgUrl": "assets/img/alienware2.png",
-      "name": "Alienware 17",
-      "category": "Gaming",
-      "price": "1299.89"
-  },
-  {
-      "id": "4",
-      "imgUrl": "assets/img/alienware4.png",
-      "name": "Alienware Special",
-      "category": "Laptop",
-      "price": "599.99"
-  },
-  {
-      "id": "5",
-      "imgUrl": "assets/img/alienware3.png",
-      "name": "Alienware 2021",
-      "category": "Laptop",
-      "price": "1799.89"
-  },
-  {
-      "id": "6",
-      "imgUrl": "assets/img/alienware6.png",
-      "name": "Alienware Desktop",
-      "category": "Desktop",
-      "price": "1899.89"
-  },
-  {
-      "id": "7",
-      "imgUrl": "assets/img/alienware7.png",
-      "name": "Alienware Kit",
-      "category": "Desktop",
-      "price": "899.89"
-  }
-]
-
-const { products: productsList} = Products(products)
-console.log(productsList.getMappedProductsByKey(sortParams))
-console.log(productsList.getMappedProductsByCategory())
+// const sortParams:sortParamsType = {
+//   price: 1,
+//   category: 1,
+//   name: 1,
+//   id: 1
+// }
